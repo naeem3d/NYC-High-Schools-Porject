1b468ec35a4c59a3ff1b8e8d9943f1154a599a10
Naeem <naeem3d@gmail.com>
2023-08-24 13:38:34 -0400

    I replace main view by school list

--- NYC_HighSchoolsSwiftUI/NYC_HighSchoolsSwiftUI/NYC_HighSchoolsSwiftUIApp.swift
+++ NYC_HighSchoolsSwiftUI/NYC_HighSchoolsSwiftUI/NYC_HighSchoolsSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct NYC_HighSchoolsSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SchoolListView()
         }
     }
 }
